2993790bcb7dd358b6bd98530d6ed3356fe2a7b5
Andrey Pereverzin <bertoluCCi>
2013-06-12 20:46:38 +0100

    API update.

--- src/main/java/com/creek/whereareyoumodel/repository/ContactResponseRepository.java
+++ src/main/java/com/creek/whereareyoumodel/repository/ContactResponseRepository.java
@@ -8,7 +8,7 @@ import com.creek.whereareyoumodel.domain.sendable.ContactResponse;
  * 
  * @author Andrey Pereverzin
  */
-public interface ContactResponseRepository extends IdentifiableRepository<ContactResponse> {
+public interface ContactResponseRepository<T extends ContactResponse> extends IdentifiableRepository<T> {
     List<ContactResponse> getAllContactResponses();
     List<ContactResponse> getContactResponsesByContactId(String contactId);
     List<ContactResponse> getContactResponsesByEmail(String email);
